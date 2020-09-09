7c001edec7568fe7ec9912e5b923b45043dce111
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-09-09 16:38:15 -0400

    one more

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -224,7 +224,7 @@ class CarInterface(CarInterfaceBase):
 
     events = self.create_common_events(ret)
 
-    ret.enableCruise = (not ret.openpilotLongitudinalControl) or (not self.CC.longcontrol)
+    ret.enableCruise = (not self.CP.openpilotLongitudinalControl) or (not self.CC.longcontrol)
     ret.brakeHold = self.CS.brakeHold != 0
     ret.parkBrake = self.CS.parkBrake != 0
 
