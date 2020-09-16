1d1dbfd7fdb21c366097b6baa41c23e962916fff
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-09-15 21:30:50 -0400

    update

--- panda/board/safety/safety_defaults.h
+++ panda/board/safety/safety_defaults.h
@@ -1,12 +1,30 @@
-bool hyundai_community_non_scc_car = false;
+int mdps_msg_count = 0;
+bool hyundai_community_non_scc_car = true;
 bool hyundai_community_mdps_harness_present = false;
 
 int default_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
   int bus = GET_BUS(to_push);
   int addr = GET_ADDR(to_push);
 
-  if ((bus == 0) && (addr == 593 || addr == 897)) {
-    hyundai_community_mdps_harness_present = false;
+  if (bus == 1) {
+     if ((addr == 593) && (mdps_msg_count == 0)){
+       mdps_msg_count += 1
+     }
+     if ((addr == 897) && (mdps_msg_count == 1)){
+       mdps_msg_count += 1
+     }
+     if ((addr == 688) && (mdps_msg_count == 2)){
+       mdps_msg_count += 1
+     }
+     if ((addr == 1296) || (addr == 524) || (addr == 1554)) && (mdps_msg_count > 0)){
+       mdps_msg_count -= 3
+     }
+     if (mdps_msg_count > 0) {
+       hyundai_community_mdps_harness_present = true;
+     }
+     else {
+       hyundai_community_mdps_harness_present = false;
+     }
   }
 
   if ((bus == 0) && ((addr == 1056) || (addr == 1057) || (addr == 1290) || (addr == 905))) {
